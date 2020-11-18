b2a650444f59b892a8083584a377f8a9ccd603a4
Nikita Sidorevich <nikitasidorevich@gmail.com>
2020-11-18 16:01:37 +0300

    Initial commit

---
+++ src/test/java/com/zebrunner/DemoTest.java
@@ -0,0 +1,19 @@
+package com.zebrunner;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DemoTest {
+
+    @Test
+    public void firstTest() {
+        assertTrue(Boolean.TRUE);
+    }
+
+    @Test
+    public void secondTest() {
+        assertTrue(Boolean.TRUE);
+    }
+
+}
