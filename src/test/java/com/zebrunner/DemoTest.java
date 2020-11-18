93ca03a6ae5fe193e09daeabf57c900c7cddfc4c
Nikita Sidorevich <nikitasidorevich@gmail.com>
2020-11-18 16:48:40 +0300

    004. Logging - log4j2

--- src/test/java/com/zebrunner/DemoTest.java
+++ src/test/java/com/zebrunner/DemoTest.java
@@ -1,23 +1,27 @@
 package com.zebrunner;
 
-import com.zebrunner.agent.core.annotation.JiraReference;
 import com.zebrunner.agent.core.annotation.Maintainer;
-import com.zebrunner.agent.core.annotation.Priority;
 import com.zebrunner.agent.core.annotation.TestLabel;
 import com.zebrunner.agent.core.registrar.Label;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Maintainer("admin")
 public class DemoTest {
 
+    private static final Logger log = LoggerFactory.getLogger(DemoTest.class);
+
     @Test
-    @Disabled
     @DisplayName("First")
     public void firstTest() {
+        for (int i = 0; i < 10; i++) {
+            log.info("Here goes my log record captured at {}", System.currentTimeMillis());
+        }
+//        waitHooks();
         assertTrue(Boolean.TRUE);
     }
 
@@ -25,16 +29,18 @@ public class DemoTest {
     @TestLabel(name = "framework", value = "junit5")
     @Maintainer("anonymous")
     public void secondTest() {
+        log.warn("I feel warned at this point.");
         Label.attach("attached-via", "test-body");
+//        waitHooks();
         assertTrue(Boolean.TRUE);
     }
 
-    @Test
-    @DisplayName("Third")
-    @Priority(Priority.P4)
-    @JiraReference("ZEB-0001")
-    public void thirdTest() {
-        assertTrue(Boolean.FALSE);
+    private void waitHooks() {
+        try {
+            Thread.sleep(2000); // workaround for shutdown hooks issue
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
