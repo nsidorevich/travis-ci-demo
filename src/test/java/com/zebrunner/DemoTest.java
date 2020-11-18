393f9f7842d538c8810e030be01a3fbdfcd5f528
Nikita Sidorevich <nikitasidorevich@gmail.com>
2020-11-18 16:18:56 +0300

    003. Labels (custom, predefined, test-case mgmt)

--- src/test/java/com/zebrunner/DemoTest.java
+++ src/test/java/com/zebrunner/DemoTest.java
@@ -1,6 +1,11 @@
 package com.zebrunner;
 
+import com.zebrunner.agent.core.annotation.JiraReference;
 import com.zebrunner.agent.core.annotation.Maintainer;
+import com.zebrunner.agent.core.annotation.Priority;
+import com.zebrunner.agent.core.annotation.TestLabel;
+import com.zebrunner.agent.core.registrar.Label;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,19 +15,24 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class DemoTest {
 
     @Test
+    @Disabled
     @DisplayName("First")
     public void firstTest() {
         assertTrue(Boolean.TRUE);
     }
 
     @Test
+    @TestLabel(name = "framework", value = "junit5")
     @Maintainer("anonymous")
     public void secondTest() {
+        Label.attach("attached-via", "test-body");
         assertTrue(Boolean.TRUE);
     }
 
     @Test
     @DisplayName("Third")
+    @Priority(Priority.P4)
+    @JiraReference("ZEB-0001")
     public void thirdTest() {
         assertTrue(Boolean.FALSE);
     }
