34c16dc582bee1b2e6c2725490f58b7cd792a80d
Nikita Sidorevich <nikitasidorevich@gmail.com>
2020-11-18 16:15:50 +0300

    002. Maintainer and test name override

--- src/test/java/com/zebrunner/DemoTest.java
+++ src/test/java/com/zebrunner/DemoTest.java
@@ -1,19 +1,30 @@
 package com.zebrunner;
 
+import com.zebrunner.agent.core.annotation.Maintainer;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Maintainer("admin")
 public class DemoTest {
 
     @Test
+    @DisplayName("First")
     public void firstTest() {
         assertTrue(Boolean.TRUE);
     }
 
     @Test
+    @Maintainer("anonymous")
     public void secondTest() {
         assertTrue(Boolean.TRUE);
+    }
+
+    @Test
+    @DisplayName("Third")
+    public void thirdTest() {
+        assertTrue(Boolean.FALSE);
     }
 
 }
