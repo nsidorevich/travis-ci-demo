6a8ee53203e21a346aaeccc82b09b21903167720
Nikita Sidorevich <nikitasidorevich@gmail.com>
2021-02-09 15:29:42 +0200

    Sorting applied

--- src/test/java/com/zebrunner/Sample.java
+++ src/test/java/com/zebrunner/Sample.java
@@ -2,13 +2,19 @@ package com.zebrunner;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Sample {
 
     @Test
     public void test() {
-        System.getenv().entrySet().forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
+        Map<String, String> envVars = System.getenv();
+        envVars.entrySet()
+               .stream()
+               .sorted()
+               .forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
         assertEquals(true, true);
     }
 
